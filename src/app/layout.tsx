aeec9d713c5e944ce6b6c2fa192c3301f56f249f
nayiswftw <pronaybiswas13407@gmail.com>
2025-10-14 08:51:36 +0530

    light theme default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en" className={`${geist.variable}`} suppressHydrationWarning>
 				<body>
-					<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<ThemeProvider attribute="class" defaultTheme="light" enableSystem>
 						<TRPCReactProvider>{children}</TRPCReactProvider>
 						<Toaster />
 					</ThemeProvider>
